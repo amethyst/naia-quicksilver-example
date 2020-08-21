852a652460e75745326b58adf81ff990580bbe84
Connor Carpenter <naia.rust.crate@gmaiol.com>
2020-08-20 22:28:25 -0600

    cleanup

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,8 +9,6 @@ mod app;
 use app::{app, get_settings};
 
 fn main() {
-    // Uncomment the line below to enable logging. You don't need it if something
-    // else (e.g. quicksilver) is logging for you
     simple_logger::init_with_level(log::Level::Info).expect("A logger was already initialized");
 
     run(get_settings(), app)
--- server/src/main.rs
+++ server/src/main.rs
@@ -24,10 +24,6 @@ async fn main() {
 
     let mut server_config = ServerConfig::default();
     server_config.heartbeat_interval = Duration::from_secs(2);
-    // Keep in mind that the disconnect timeout duration should always be at least
-    // 2x greater than the heartbeat interval, to make it so at the worst case, the
-    // server would need to miss 2 heartbeat signals before disconnecting from a
-    // given client
     server_config.disconnection_timeout_duration = Duration::from_secs(5);
 
     let mut server = NaiaServer::new(
@@ -38,9 +34,6 @@ async fn main() {
     )
     .await;
 
-    // This method is called during the connection handshake process, and can be
-    // used to reject a new connection if the correct credentials have not been
-    // provided
     server.on_auth(Rc::new(Box::new(|_, auth_type| {
         if let ExampleEvent::AuthEvent(auth_event) = auth_type {
             let username = auth_event.username.get();
@@ -50,17 +43,12 @@ async fn main() {
         return false;
     })));
 
-    // Create a new, singular room, which will contain Users and Entities that they
-    // can receive updates from
     let main_room_key = server.create_room();
 
-    // Create 4 PointEntities, with a range of X values
     let main_entity = PointEntity::new(16,16).wrap();
     let entity_key = server.register_entity(main_entity);
     server.room_add_entity(&main_room_key, &entity_key);
 
-    // This method will be called every step to determine whether a given Entity
-    // should be in scope for a given User
     server.on_scope_entity(Rc::new(Box::new(|_, _, _, entity| match entity {
         ExampleEntity::PointEntity(point_entity) => {
             return true;
@@ -93,11 +81,6 @@ async fn main() {
                         }
                     }
                     ServerEvent::Tick => {
-                        // Game logic, updating of the world, should happen here
-
-                        // VERY IMPORTANT! Calling this actually sends all Entity/Event data packets
-                        // to all Clients that require it. If you don't call this method, the Server
-                        // will never communicate with it's connected Clients
                         server.send_all_updates().await;
                     }
                 }
